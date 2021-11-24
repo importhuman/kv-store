aca0464ae7878dc83362c182ec51209ded24c7f4
Ujjwal Goyal <importujjwal@gmail.com>
2021-11-24 22:17:38 +0530

    Implement zero downtime deployment

--- main.go
+++ main.go
@@ -10,18 +10,8 @@ import (
 )
 
 func main() {
-
-	// var store = map[string]interface{}{
-	// 	"abc": 1,
-	// 	"xyz": 2,
-	// }
-
-	// for k, v := range store {
-	// 	fmt.Printf()
-	// }
-
 	r := mux.NewRouter()
-	r.HandleFunc("/get", kv.GetAll).Methods("GET")
+	r.HandleFunc("/", kv.GetAll).Methods("GET")
 	r.HandleFunc("/get/{key}", kv.Get).Methods("GET")
 	r.HandleFunc("/set", kv.Set).Methods("POST")
 	r.HandleFunc("/search", kv.Search).Queries("prefix", "{str}").Methods("GET")
